a2b08ec72e6185866391db7d8c0447796bb4596c
“EricaIna” <erica.rirura.4you@gmail.com>
2024-01-09 17:54:41 +0000

    loginform changes applied

--- src/Components/LoginForm/index.jsx
+++ src/Components/LoginForm/index.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
-const LoginForm = () => {
-  const [username, setUsername] = useState("");
+const LoginForm = ({ onLogin }) => {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [successMessage, setSucessMessage] = useState("");
-  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(email, password);
+  };
+
   return (
-    <>
-      <form type="submit">
-        <input type="text" placeholder="Username" />
-        <input type="text" placeholder="Password" />
-      </form>
-    </>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
   );
 };
 
