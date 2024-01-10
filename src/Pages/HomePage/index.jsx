cb3b38ed28d981e4091b6cb2cd4d90c7fa171671
“EricaIna” <erica.rirura.4you@gmail.com>
2024-01-10 21:39:55 +0000

    background

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -3,12 +3,17 @@ import React, { useState, useEffect } from "react";
 import { Movies } from "../../Components/Movies";
 import "./HomePage.css";
 import { Upcoming } from "../../Components/Upcoming";
+import backgroundVideo from "../../assets/bc.mp4";
 
 const HomePage = () => {
   return (
     <>
       <div className="homePage">
         <div className="background">
+          <video autoPlay loop muted>
+            <source src={backgroundVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
           <h1 className="neon-text">MOVIE TITLE</h1>
           <em>join us now</em>
         </div>
