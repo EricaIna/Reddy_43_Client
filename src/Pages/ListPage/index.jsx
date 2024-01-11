feedb1cd02ea13831129b44895272ab3978a2e83
“EricaIna” <erica.rirura.4you@gmail.com>
2024-01-11 15:36:19 +0000

    before merge

--- src/Pages/ListPage/index.jsx
+++ src/Pages/ListPage/index.jsx
@@ -35,7 +35,7 @@ const ListPage = () => {
     genreIds.map((id) => genres[id]).filter((name) => name);
 
   return (
-    <div>
+    <div className="movies">
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
@@ -51,37 +51,3 @@ const ListPage = () => {
 };
 
 export default ListPage;
-
-// import React, { useState, useEffect } from 'react'
-
-// const ListPage = () => {
-//     const [movies, setMovies] = useState([]);
-
-//     const allMovies = async () => {
-//         try{
-//             const response = await fetch('http://localhost:4000/movies')
-//             const data = await response.json()
-//             console.log(data)
-//             setMovies(data.data.map(movie => movie.original_title));
-//     } catch (error) {
-//       console.error('Error fetching movies:', error);
-
-//     }
-
-// };
-// useEffect(() => {
-//     allMovies();
-//   }, []);
-//   return (
-//     <div>
-//       <h1>List of Movies</h1>
-//       <ul>
-//         {movies.map((title, index) => (
-//           <li key={index}>{title}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default ListPage
