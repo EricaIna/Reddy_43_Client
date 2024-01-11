de911bf84c7c164bfd2aefaa6cfb8851a5efe722
“EricaIna” <erica.rirura.4you@gmail.com>
2024-01-11 14:11:37 +0000

    homepage background and btn

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,39 @@ import * as Pages from "./Pages";
 import { AnimatePresence } from "framer-motion";
 import ProtectedRoute from "./ProtectedRoute";
 
+// function App() {
+//   return (
+//     <>
+//       <AnimatePresence>
+//         <DropdownMenu />
+//         <Routes>
+//           <Route path="/" element={<ProtectedRoute><Pages.HomePage /></ProtectedRoute>}></Route>
+//           <Route path="/login" element={<Pages.LoginPage />} />
+//           <Route path="/register" element={<Pages.RegisterPage />} />
+//           <Route path="/list" element={<ProtectedRoute><Pages.ListPage /></ProtectedRoute>} />
+//         </Routes>
+//       </AnimatePresence>
+//     </>
+//   );
+// }
+
 function App() {
   return (
     <>
       <AnimatePresence>
         <DropdownMenu />
         <Routes>
-          <Route path="/" element={<ProtectedRoute><Pages.HomePage /></ProtectedRoute>}></Route>
+          <Route path="/" element={<Pages.HomePage />}></Route>
           <Route path="/login" element={<Pages.LoginPage />} />
           <Route path="/register" element={<Pages.RegisterPage />} />
-          <Route path="/list" element={<ProtectedRoute><Pages.ListPage /></ProtectedRoute>} />
+          <Route
+            path="/list"
+            element={
+              <ProtectedRoute>
+                <Pages.ListPage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AnimatePresence>
     </>
