cc1eff708195314009c9568e531d8b4f21e9d684
Billy <s.pbilly@yahoo.co.uk>
2024-01-12 11:44:36 +0000

    add upcoming and top page

---
+++ src/Pages/UpcomingPage/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { MovieCard } from "../../Components";
+
+const UpcomingPage = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/movies/upcoming");
+        const data = await response.json();
+        console.log(data);
+        setMovies(data.results);
+      } catch (error) {
+        console.log("Error Fetching Data");
+      }
+    };
+    fetchMovies();
+  }, []);
+
+
+  return (
+    <div className="movies">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          poster={movie.poster_path}
+          summary={movie.overview}
+          year={movie.release_date}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default UpcomingPage;
