e2ccd3fdd898572a55c6d6555fc51ccfe27c884c
“EricaIna” <erica.rirura.4you@gmail.com>
2024-01-14 22:54:13 +0000

    header/drop down fixed on top, register/login miner change

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import * as Pages from "./Pages";
 
 import ProtectedRoute from "./ProtectedRoute";
 import { ScrollableSection } from "./Components";
+import { Header } from "./Components/Header";
 
 // function App() {
 //   return (
@@ -26,24 +27,26 @@ import { ScrollableSection } from "./Components";
 function App() {
   return (
     <>
-      <DropdownMenu />
+      {/* <DropdownMenu /> */}
       <Routes>
-        <Route path="/" element={<Pages.HomePage />}></Route>
-        <Route path="/login" element={<Pages.LoginPage />} />
-        <Route path="/register" element={<Pages.RegisterPage />} />
-        <Route
-          path="/list"
-          element={
-            // <ProtectedRoute>
-            <Pages.ListPage />
-            // </ProtectedRoute>
-          }
-        />
-        <Route path="/list" element={<Pages.ListPage />} />
-        <Route path="/genre" element={<Pages.GenrePage />} />
-        <Route path="/top" element={<Pages.TopPage />} />
-        <Route path="/upcoming" element={<Pages.UpcomingPage />} />
-        <Route path="/scroll" element={<ScrollableSection />} />
+        <Route path="/" element={<Header />}>
+          <Route path="/" element={<Pages.HomePage />}></Route>
+          <Route path="/login" element={<Pages.LoginPage />} />
+          <Route path="/register" element={<Pages.RegisterPage />} />
+          <Route
+            path="/list"
+            element={
+              // <ProtectedRoute>
+              <Pages.ListPage />
+              // </ProtectedRoute>
+            }
+          />
+          <Route path="/list" element={<Pages.ListPage />} />
+          <Route path="/genre" element={<Pages.GenrePage />} />
+          <Route path="/top" element={<Pages.TopPage />} />
+          <Route path="/upcoming" element={<Pages.UpcomingPage />} />
+          <Route path="/scroll" element={<ScrollableSection />} />
+        </Route>
       </Routes>
     </>
   );
