00efb03400bdeca413420d0ead968d05865882de
“EricaIna” <erica.rirura.4you@gmail.com>
2024-01-15 22:06:04 +0000

    upcoming page layout,dropdown miner change

--- src/Pages/UpcomingPage/index.jsx
+++ src/Pages/UpcomingPage/index.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { MovieCard } from "../../Components";
+import "./UpcomingPage.css";
 
 const UpcomingPage = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-
     const fetchMovies = async () => {
       try {
         const response = await fetch("http://localhost:4000/movies/recent");
@@ -19,18 +19,21 @@ const UpcomingPage = () => {
     fetchMovies();
   }, []);
 
-
   return (
-    <div className="movies">
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          title={movie.title}
-          poster={`https://image.tmdb.org/t/p/w500https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          summary={movie.overview}
-          year={movie.release_date}
-        />
-      ))}
+    <div className="upcoming-page">
+      <h1 className="upcoming-h1">Upcoming and Popular</h1>
+      <div className="upcoming-movies">
+        {movies.map((movie) => (
+          <MovieCard
+            className="upcoming-movie"
+            key={movie.id}
+            title={movie.title}
+            poster={`https://image.tmdb.org/t/p/w500https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            summary={movie.overview}
+            year={movie.release_date}
+          />
+        ))}
+      </div>
     </div>
   );
 };
