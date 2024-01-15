583e54dc3df4d703a2323d115c9f6a5c9c1f4b4e
Joseph Barnett <joseph-barnett@outlook.com>
2024-01-15 18:03:37 +0000

    merge

--- src/Components/MovieCard/index.jsx
+++ src/Components/MovieCard/index.jsx
@@ -1,28 +1,8 @@
-// import React from "react";
-
-// const MovieCard = ({ title, poster, summary, year, genre }) => {
-//   return (
-//     <div role="card" className="card">
-//       <img src={poster} alt={title} />
-//       <h3>{title}</h3>
-//       <p className="summary">{summary}</p>
-//       <p>{year.substring(0,4)}</p>
-//       <p>{genre}</p>
-//     </div>
-//   );
-// };
-
-// export default MovieCard;
-
 import React from "react";
 
 export const MovieCard = ({ title, poster, summary, year, genre, onClick }) => {
   return (
-<<<<<<< HEAD
-    <div className="card">
-=======
     <div role="card" className="card" onClick={onClick}>
->>>>>>> 40f4813efdfc8792b598f7320a51e35d0b99052f
       <img src={poster} alt={title} />
       <h3>{title}</h3>
       {/* <p className="summary">{summary}</p> */}
