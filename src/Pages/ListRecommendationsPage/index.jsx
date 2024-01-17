2a75ff4a794fca6bc76a2a191a2e064dca8d35c6
“EricaIna” <erica.rirura.4you@gmail.com>
2024-01-17 13:08:11 +0000

    recommendation page styling

--- src/Pages/ListRecommendationsPage/index.jsx
+++ src/Pages/ListRecommendationsPage/index.jsx
@@ -5,54 +5,55 @@ import { FilmCard } from "../../Components";
 import { useNavigate } from "react-router-dom";
 import "./ListRecommendationsPage.css";
 
-
-
 const ListRecommendationsPage = () => {
   const { id } = useParams();
 
   const [list, setList] = useState([]);
 
   const navigate = useNavigate();
-  
-  useEffect(() => {
 
+  useEffect(() => {
     const fetchListsById = async () => {
       try {
         const options = {
-            method: 'GET',
-            headers: {
-                
-                'Authorization': 'Bearer ' +localStorage.getItem("accessToken")
-            }
-        }
-             
-        const response = await fetch(`http://localhost:4000/recommendations_list/${id}`,options);
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
+          },
+        };
+
+        const response = await fetch(
+          `http://localhost:4000/recommendations_list/${id}`,
+          options
+        );
         const data = await response.json();
         console.log("After fetching Data");
         // console.log("Data=",data.movies[0].title);
-        console.log("Data=",data);
+        console.log("Data=", data);
         setList(data);
       } catch (error) {
-        console.log("Error Fetching Data ",error);
+        console.log("Error Fetching Data ", error);
       }
     };
-   
+
     fetchListsById();
   }, []);
 
   const handleRecomend = async () => {
     try {
       const options = {
-        method: 'PATCH',
+        method: "PATCH",
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
         },
       };
 
-      const response = await fetch(`http://localhost:4000/recommendations_list_recommend/${id}`, options);
+      const response = await fetch(
+        `http://localhost:4000/recommendations_list_recommend/${id}`,
+        options
+      );
       window.location.reload();
-
     } catch (error) {
       console.log("Error recommending a movie", error);
     }
@@ -62,73 +63,81 @@ const ListRecommendationsPage = () => {
     navigate(`/search/${id}`);
   };
 
-const removeMovie = (movies_list, movie_index) => {
-    const newMovieList=JSON.parse(JSON.stringify(movies_list))
-    newMovieList.movies.splice(movie_index,1)
-    newMovieList.movies_id.splice(movie_index,1)
-    return newMovieList
-};
+  const removeMovie = (movies_list, movie_index) => {
+    const newMovieList = JSON.parse(JSON.stringify(movies_list));
+    newMovieList.movies.splice(movie_index, 1);
+    newMovieList.movies_id.splice(movie_index, 1);
+    return newMovieList;
+  };
 
-// Deleteng movie from the list
-  const handleDeleteFromList = async (id,movie_id) => {
+  // Deleteng movie from the list
+  const handleDeleteFromList = async (id, movie_id) => {
     try {
       const options = {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-          'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
         },
       };
-      
-      const response = await fetch(`http://localhost:4000/recommendations_list_manage/${id}/${movie_id}`, options);
+
+      const response = await fetch(
+        `http://localhost:4000/recommendations_list_manage/${id}/${movie_id}`,
+        options
+      );
       const data = await response.json();
       console.log("response after deleting=", data);
       if (response.ok) {
         // Remove the deleted item from the list
-        console.log("list.movies_id = ", list.movies_id)
-        console.log("list.movies = ", list.movies)
-        const movie_index = list.movies_id.indexOf(movie_id)
+        console.log("list.movies_id = ", list.movies_id);
+        console.log("list.movies = ", list.movies);
+        const movie_index = list.movies_id.indexOf(movie_id);
         setList((prevList) => removeMovie(prevList, movie_index));
         console.log(`Movie with ID ${movie_id} deleted successfully`);
       } else {
-        console.log(`Error deleting movie with ID ${movie_id} in the list ${id}`);
+        console.log(
+          `Error deleting movie with ID ${movie_id} in the list ${id}`
+        );
       }
     } catch (error) {
       console.log(`Error Deleting from List  ${id}`, error);
     }
   };
 
-
   return (
     <>
-        <h2 className="list-h1">{list.title}</h2>
-
-
-    <div className="list-and-form">
-      {list.movies?.map((movie, index) => (
-        <FilmCard
-            key={index}  
-            id={id}        
-            movie_id={list.movies_id?.[index]}
-            title={movie.title}
-            onDelete={handleDeleteFromList}
-            
-        
-        />
-      ))}
-
-      <div className="buttons-rec">
-        <button type="button" className="button-recommend"  onClick={handleRecomend}>
-            Recomend a movie
+      <h2 className="personal-list">{list.title}</h2>
+      <div className="personalList-area">
+        <div className="buttons-rec">
+          <button
+            type="button"
+            className="button-recommend"
+            onClick={handleRecomend}
+          >
+            Recommend me a movie
           </button>
 
-        <button type="button" className="button-recommend"  onClick={handleSearch}>
-            Search a movie to add into the list
+          <button
+            type="button"
+            className="button-recommend"
+            onClick={handleSearch}
+          >
+            Search for a movie and add to list
           </button>
+        </div>
+        <div className="list-and-form">
+          {list.movies?.map((movie, index) => (
+            <FilmCard
+              key={index}
+              id={id}
+              movie_id={list.movies_id?.[index]}
+              title={movie.title}
+              onDelete={handleDeleteFromList}
+            />
+          ))}
+        </div>
       </div>
-      </div>
-
     </>
-  )
+  );
 };
 
 export default ListRecommendationsPage;
