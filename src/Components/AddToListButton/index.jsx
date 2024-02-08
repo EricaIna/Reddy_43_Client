1bd1a970b0c26b4b0059dd90ac14e03d134e1aaf
“EricaIna” <erica.rirura.4you@gmail.com>
2024-02-08 17:38:03 +0000

    add s

--- src/Components/AddToListButton/index.jsx
+++ src/Components/AddToListButton/index.jsx
@@ -5,14 +5,17 @@ const AddToListButton = ({ movieId, onSuccess }) => {
   const handleAddToList = async () => {
     try {
       const token = localStorage.getItem("accessToken");
-      const response = await fetch("http://moviestest-env-4.eba-t3hctzae.eu-west-2.elasticbeanstalk.com/user-film-list/add", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ movies_id: movieId }),
-      });
+      const response = await fetch(
+        "https://moviestest-env-4.eba-t3hctzae.eu-west-2.elasticbeanstalk.com/user-film-list/add",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ movies_id: movieId }),
+        }
+      );
 
       const data = await response.json();
       console.log(data.message);
